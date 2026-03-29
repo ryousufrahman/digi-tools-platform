ecbe280baf65f5371d45cd718103c8b0c5ec58f8
yousufrahman <ryousuf766@gmail.com>
2026-03-29 21:56:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+    <header>
+       <Navbar></Navbar>
+    </header>
+    
+      
+    </>
+  )
+}
+
+export default App
