e1a232af94a19b88a17955cbef5df1302f6e159d
yousufrahman <ryousuf766@gmail.com>
2026-03-29 23:31:46 +0600

    added the product button section draft

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
+import ProductButton from './Components/Product/ProductButton'
 import UsersCount from './Components/UsersCount'
 
 function App() {
@@ -16,6 +17,10 @@ function App() {
       <Hero></Hero>
       <section className='bg-amber-400 mt-15  py-5 md:py-15'>
         <UsersCount></UsersCount>
+      </section>
+      <section className='product-button-section mt-5 lg:mt-20'>
+        <ProductButton></ProductButton>
+      
       </section>
     </main>
     
---
+++ src/Components/Product/ProductButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductButton = () => {
+    return (
+        <>
+         <div className='main-container p-3 max-w-300 mx-auto space-y-4'>
+            <h2 className='text-center text-3xl font-bold sm:text-4xl text-black'>Premium Digital Tools</h2>
+            <p className='text-center text-[#627382]'>Choose from our curated collection of premium digital products designed <br />to boost your productivity and creativity.</p>
+
+         </div>
+        
+        
+        </>
+    );
+};
+
+export default ProductButton;
